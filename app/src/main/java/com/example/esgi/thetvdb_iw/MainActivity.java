b9294547e710ce87129217c622628c11afdf74ae
LeMayhem <lemay.michael@icloud.com>
2017-12-14 08:38:05 +0100

    [Index]: Initialization of the project

---
+++ app/src/main/java/com/example/esgi/thetvdb_iw/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.esgi.thetvdb_iw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
